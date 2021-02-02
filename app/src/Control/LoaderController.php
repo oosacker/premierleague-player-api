5e1df320b3da8ccb45f710055a390327c9b15ee8
Natsuki Hasegawa <nat@sunnysideup.co.nz>
2021-02-02 20:02:53 +1300

    wip

--- app/src/Control/LoaderController.php
+++ app/src/Control/LoaderController.php
@@ -9,17 +9,17 @@ use SilverStripe\Forms\FormAction;
 use SilverStripe\Forms\RequiredFields;
 use SilverStripe\Control\Controller;
 
-class LoadController extends Controller 
+class LoaderController extends Controller 
 {
     private $mydata = [];
-    
+
     private static $allowed_actions = 
     [
         'Form',
         'test',
     ];
 
-    private static $url_segment = 'load';   // need this or else the form will be at /Form instead of /load/Form and will show 404 error
+    private static $url_segment = 'loader';   // need this or else the form will be at /Form instead of /load/Form and will show 404 error
 
 
     public function index()
@@ -44,12 +44,36 @@ class LoadController extends Controller
         return $form;
     }
 
+
     public function doUpload($data, $form) 
     {
+        
+        $file = $_FILES['CsvFile']['tmp_name'];
+        $handle = fopen($file, "r");
+        $big_array = []; 
+
+        if ($handle !== FALSE) {
+            $data = fgetcsv($handle);
+            while ($data !== FALSE) 
+            {		
+                $big_array = array_merge($big_array, $data);
+                $data = fgetcsv($handle);
+            }
+            fclose($handle);
+        } 
+        else {
+            die('<br>fail');
+        }
+        
         echo('<pre>');
-        var_dump($data);
+        var_dump($big_array);
         echo('</pre>');
-        die('hi');
+        die('<br>hi');
+    }
+
+    public function loadData()
+    {
+        // https://docs.silverstripe.org/en/4/developer_guides/model/relations/
     }
 
     public function test()
